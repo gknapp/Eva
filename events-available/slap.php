d207f24df082b7172229ee34bff7ca426b70c601
Greg K <virtual.greg@gmail.com>
2010-11-21 13:27:40 +0000

    Add primitive grammar analysis of item

--- events-available/slap.php
+++ events-available/slap.php
@@ -27,12 +27,29 @@ class Event_Slap extends Event_Base {
 		$nickname = $this->response->matches[1];
 		$item = 'frying pan';
 		
-		if (isset($this->response->matches[2])) {
+		if (!empty($this->response->matches[2])) {
 			$item = $this->response->matches[2];
+			$prefix = 'a ';
+			
+			// small bit of analysis on the item
+			list($word, $rest) = explode(' ', strtolower($item), 2);
+			
+			// strip 'a' prefix
+			if ($word == 'with' || $word == 'my' || $word = 'a') {
+				$prefix = '';
+			}
+			
+			if ($word != 'with') {
+				$rest = $item;
+			}
+			
+			// substitute 'my' with author's nick
+			$rest = str_replace('my', $this->response->nick . "'s", $rest);
+			$item = $prefix . $rest;
 		}
-	
+		
 		$this->bot->action(
-			"slaps $nickname around with a $item",
+			"slaps $nickname around with $item",
 			$this->response->target
 		);
 	
