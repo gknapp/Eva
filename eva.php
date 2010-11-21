d76c78def9be7a17f30f4db030979ac78f404d2c
Greg K <virtual.greg@gmail.com>
2010-11-21 00:18:45 +0000

    Add text color support and shortcut methods for common IRC commands

--- eva.php
+++ eva.php
@@ -12,9 +12,50 @@ class Eva extends Drone {
 	
 	}
 	
+	public function say($msg, $target) {
+	
+		if (!empty($this->cfg['textcolor'])) {
+			$c = $this->cfg['textcolor'];
+			$msg = "\x03" . $c . $msg . "\x03";
+		}
+	
+		$this->client->say($msg, $target);
+	
+	}
+	
+	public function privateMessage($msg, $target) {
+	
+		$this->client->say($msg, $target);
+	
+	}
+	
 	public function action($action, $channel) {
 	
 		$this->client->action($action, $channel);
+	
+	}
+	
+	public function join($channel) {
+	
+		$this->client->join($channel);
+	
+	}
+	
+	public function part($channel) {
+	
+		$this->client->part($channel);
+	
+	}
+	
+	public function notice($msg, $target) {
+	
+		$this->client->notice($msg, $target);
+	
+	}
+	
+	public function quit($msg = 'kthxbye') {
+	
+		$this->client->quit($msg);
 	
 	}
 
